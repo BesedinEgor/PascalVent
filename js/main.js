ac543e62e9feb2bad8fbe63d3f32a2a8b1389578
Egor <besedinegor@mail.ru>
2023-02-10 22:36:54 +0300

    реализация двух блоков, реализация переключение табов с различным контентом в секции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //открытие/закрытие меню
 const burgerBtn = document.querySelector(".header__burger-btn");
-const nav = document.querySelector('.nav')
+const nav = document.querySelector(".nav");
 const hundlerClick = (event) => {
-    nav.classList.toggle('nav-open')
-    event.preventDefault();
-}
+  nav.classList.toggle("nav-open");
+  event.preventDefault();
+};
 
-//закрытие меню при клике вне меню 
+//закрытие меню при клике вне меню
 document.addEventListener("click", function (event) {
   const target = event.target;
   const its_menu = target == nav || nav.contains(target);
@@ -18,4 +18,32 @@ document.addEventListener("click", function (event) {
   }
 });
 
-burgerBtn.addEventListener('click', hundlerClick)
+burgerBtn.addEventListener("click", hundlerClick);
+
+//переключение табов
+
+const tabButton = document.querySelectorAll(".services__btn");
+const tabContent = document.querySelectorAll(".services__options-content");
+
+tabButton.forEach(function (element) {
+  element.addEventListener("click", openContent);
+});
+
+function openContent(evt) {
+  const tabTarget = evt.currentTarget; //элемент по которому нажали
+  const button = tabTarget.dataset.button; //кнопка с дата-атрибутом
+
+  tabButton.forEach(function (item) {
+    item.classList.remove("services__btn--active");
+  });
+
+  tabContent.forEach(function (item) {
+    item.classList.remove("services__options-content--active");
+  });
+
+  tabTarget.classList.add("services__btn--active");
+
+  document
+    .querySelector(`#${button}`)
+    .classList.add("services__options-content--active");
+}
