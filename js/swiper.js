b57d37f8410125dfdb8e61edca62d7333195b102
Egor <besedinegor@mail.ru>
2023-02-16 21:45:56 +0300

    начат блок портфолио

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".portfolio-swiper", {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
