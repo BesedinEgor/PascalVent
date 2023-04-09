c1b103ee05b3c9c09c4136bc9394be6dc86b612c
Egor Besedin <Besedinegor@mail.ru>
2023-04-09 21:40:01 +0300

    фикс прыжка в момент появления фиксированного меню, добавления файла стилей(swiper-bundle.min) для swiper

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,20 @@
 //фиксирование шапки при скролле лендинга
 const headerNav = document.querySelector('.header__container');
-const first = document.querySelector('.main');
+const first = document.querySelector('.services');
+const header = document.querySelector('header')
 
 const headerHeight = headerNav.offsetHeight;
 const firstHeight = first.offsetHeight;
+console.log(headerHeight);
 
 window.addEventListener('scroll', () => {
   let scrollDistance = window.scrollY;
 
-  if(scrollDistance >= 918) {
-    headerNav.classList.add('header--fixed');
+  if (scrollDistance >= firstHeight + headerHeight) {
+    headerNav.classList.add("header--fixed");
+    header.classList.add('header--fix');
   } else {
-    headerNav.classList.remove('header--fixed');
+    headerNav.classList.remove("header--fixed");
+    header.classList.remove("header--fix");
   }
 });
